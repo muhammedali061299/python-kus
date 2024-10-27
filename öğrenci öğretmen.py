3600d7b21e15f420c391ab47901204f0ec80358e
muhammedali061299 <barbarossa20001299@gmail.com>
2024-10-27 12:52:50 +0300

    27.10
    çeği

---
+++ öğrenci öğretmen.py
@@ -0,0 +1,40 @@
+class  ogrenci(): #kalıp
+     adı="--"
+     ortalaması=50
+
+ogrenci1=ogrenci()
+ogrenci1.adi="Buse Doğan"
+ogrenci1.ortalaması=80
+
+ogrencı2=ogrenci()
+ogrencı2.adi="Ebru Gündeş"
+ogrenci1.ortalaması=90
+
+
+print(ogrencı2)
+print(ogrencı2.adi)
+print(ogrencı2.soyadi)
+
+
+
+class  ogretmen(): #kalıp
+     adı="--"
+     soyadı="--"
+     sınıf="--"
+     brans="--"
+
+ogretmen1=ogretmen()
+ogretmen1.adi="Kuraner"
+ogretmen1.soyadı="Tuna"
+ogretmen1.sınıf="4b"
+ogretmen1.brans="sınıf oğretmenı"
+
+ogretmen2=ogretmen()
+ogretmen2.adi="Emilhan Yılmaz"
+ogretmen2.oratalaması=80
+
+print(ogretmen2)
+print(ogretmen.adi)
+print(ogretmen.soyadi)
+
+
